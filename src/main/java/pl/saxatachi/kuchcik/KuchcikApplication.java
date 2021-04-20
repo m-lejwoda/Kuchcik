eb4110687e177d2ef89470ec628b84a43b369162
saxatachi <saxatachi@gmail.com>
2021-04-20 19:51:34 +0200

    init

---
+++ src/main/java/pl/saxatachi/kuchcik/KuchcikApplication.java
@@ -0,0 +1,13 @@
+package pl.saxatachi.kuchcik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuchcikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuchcikApplication.class, args);
+	}
+
+}
